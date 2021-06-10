2f5460c0d224601359b2262df08a8c4a91b147a2
prajotsurey <prajotsurey@gmail.com>
2021-06-10 07:42:36 +0530

    updated structure to follow single repo deployment

--- client/src/services/loginService.js
+++ client/src/services/loginService.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios"
-const baseUrl = 'http://localhost:3001/api/login'
+const baseUrl = '/api/login'
 
 
 const login = async (credentials) => {
