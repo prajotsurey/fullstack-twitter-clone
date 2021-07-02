2a79bff9e18ea10ee9b47b036f74b2e1dbb5961d
prajot <prajotsurey@gmail.com>
2021-07-02 15:05:44 +0530

    added post design

--- client/src/components/CenterSpace/index.js
+++ client/src/components/CenterSpace/index.js
@@ -14,5 +14,4 @@ const CentreSpace = (props) => {
   )
 }
 
-
 export default CentreSpace;
--- client/src/pages/Browse/index.js
+++ client/src/pages/Browse/index.js
@@ -20,9 +20,49 @@ const BlogList = () => {
       { 
         posts.map(post => {
           return(
-            <div key={post.id} className="flex flex-col mb-1 border-b p-4">
-              <div>{post.content}</div>
-              <div className="text-xs text-green-700">blog author</div>
+            <div key={post.id} className="flex flex-row border-b p-3">
+              <div className="mr-3">
+                <div className="h-12 w-12 rounded-xl bg-green-200">
+
+                </div>
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex flex-row justify-between">
+                  <div className="">
+                    <span className="text-sm font-bold">
+                      Name 
+                    </span> 
+                    <span className="text-sm text-gray-400">
+                      @username
+                    </span>
+                    <span className="text-sm text-gray-400 pl-1">
+                      . date
+                    </span>
+                  </div>
+                  <div >
+                    <span className="text-sm text-gray-400"> 
+                      button
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  {post.content}
+                </div>
+                <div className="flex flex-row mt-3">
+                  <div className="flex-grow text-sm text-gray-400">
+                    button
+                  </div>
+                  <div className="flex-grow text-sm text-gray-400">
+                    button
+                  </div>
+                  <div className="flex-grow text-sm text-gray-400">
+                    button
+                  </div>
+                  <div className="flex-grow text-sm text-gray-400">
+                    button
+                  </div>
+                </div>
+              </div>
             </div>
           )
         })
