91b40ff507d0e0d5a911c2e4ed68f9a36750f170
prajot <prajotsurey@gmail.com>
2021-07-19 14:49:24 +0530

    updated user page

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,9 @@ const App = () => {
         </CentreSpace>
       </PrivateRoute>
       <PrivateRoute path='/profile'>
-        <Profile user={1}/>
+        <CentreSpace>
+          <Profile user={1}/>
+        </CentreSpace>
       </PrivateRoute>
       <PublicRoute path='/signup'>
         <SignUp user={1}/>
--- client/src/components/LeftSidebar/index.js
+++ client/src/components/LeftSidebar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSidebar = () => {
   return(
@@ -17,6 +18,14 @@ const LeftSidebar = () => {
         </div>
         <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
           Explore
+        </div>
+      </div>
+      <div className="flex flex-row items-center text-2xl py-3 px-3 rounded-full font-semibold hover:bg-green-50">
+        <div className="h-8 w-8 rounded-xl bg-green-200">
+          
+        </div>
+        <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
+        <Link to="/profile">Profile</Link>
         </div>
       </div>
     </div>
