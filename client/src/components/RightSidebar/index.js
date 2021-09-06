b7c7fb492ef211c9b98490f1a16ddcc8e7692067
prajotsurey <prajotsurey@gmail.com>
2021-09-06 11:10:13 +0530

    fixed search bar of right sidebar to the top

--- client/src/components/RightSidebar/index.js
+++ client/src/components/RightSidebar/index.js
@@ -18,14 +18,15 @@ const RightSidebar = () => {
 
   return(
     <div className="flex flex-col flex-grow items-start">
-      <div className="top-0 bottom-0 flex-col py-1 ml-7 mr-3 lg:items-start hidden rightShowSmall:flex rightShowSmall:w-rightSmall rightShowLarge:w-rightLarge ">
-        <div className="w-full">
+      <div className="top-0 bottom-0 flex-col ml-7 mr-3 lg:items-start hidden rightShowSmall:flex rightShowSmall:w-rightSmall rightShowLarge:w-rightLarge ">
+        {/* after adding position fixed, the search bar does not follow width properties of its parent. Added those again to make it work  */}
+        <div className="fixed h-14 bg-white flex flex-col justify-center hidden rightShowSmall:flex rightShowSmall:w-rightSmall rightShowLarge:w-rightLarge">
           <Formik
           initialValues={initialValues}
           onSubmit={(values) => {search(values)}}
           > 
             {() => (
-              <Form className="flex-col">
+              <Form className="flex-col w-full">
                 <Field name="search" placeholder="Search Twitter" component={SearchInput}/>
                 {/* <button className="rounded-md w-full bg-green-200 h-12 px-4" type="submit">Sign In</button> */}
               </Form>
@@ -33,8 +34,8 @@ const RightSidebar = () => {
             
           </Formik>
         </div>
-
-
+        {/* dummy box to add margin so content does not hide behind search box */}
+        <div className="h-14"></div>
         <div className="flex flex-col mt-3 rounded-xl bg-gray-50 w-full">
           <div className="px-4 py-3 text-xl font-semibold border-b">
             What's happening
--- client/src/components/SearchInput/index.js
+++ client/src/components/SearchInput/index.js
@@ -3,7 +3,7 @@ import { Field } from 'formik'
 const TextInput = ({field, form: {touched, errors}, placeholder, ...props}) => {
   return(
     <div className="flex-row">
-      <Field className="w-full h-10 p-4 rounded-full focus:outline-none focus:ring-1 focus:ring-green-200 focus:bg-white bg-gray-50" placeholder={placeholder} {...field}/>
+      <Field className="w-full h-12 p-4 rounded-full focus:outline-none focus:ring-1 focus:ring-green-200 focus:bg-white bg-gray-50" placeholder={placeholder} {...field}/>
       {touched[field.name] && errors[field.name] && <div className="text-red-500">{errors[field.name]}</div>}
     </div>
   )
