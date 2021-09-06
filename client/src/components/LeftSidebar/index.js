82a8a098024b35bf9f1309a5d0c4a6b4af463e85
prajotsurey <prajotsurey@gmail.com>
2021-09-06 10:57:52 +0530

    updated left sidebar added svg icons updated link positions and hover behaviour fixed left sidebar to the top

--- client/src/components/LeftSidebar/index.js
+++ client/src/components/LeftSidebar/index.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import { ReactComponent as HomeIcon } from '../../icons/HomeIcon.svg';
+import { ReactComponent as BookmarksIcon } from '../../icons/BookmarksIcon.svg';
+import { ReactComponent as ProfileIcon } from '../../icons/ProfileIcon.svg';
+import { ReactComponent as Logo } from '../../icons/LogoBlue.svg';
 import { Link } from 'react-router-dom';
+import LeftSidebarLink from '../LeftSidebarLink';
 
 const LeftSidebar = ({username}) => {
   return(
-    <div className="top-0 bottom-0 flex flex-col py-4 items-center leftShowLarge:items-start md:flex leftShowLarge:w-leftLarge w-leftSmall ">
-      <div className="px-3">
-        <div className="flex flex-row items-center text-2xl px-3 py-3 rounded-full font-semibold hover:bg-green-50">
-          <div className="h-8 w-8 rounded-xl bg-green-200">
-          </div>
-          <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
-          <Link to="/posts">Home</Link>
-          </div>
-        </div>
-        <div className="flex flex-row items-center text-2xl px-3 py-3 rounded-full font-semibold hover:bg-green-50">
-          <div className="h-8 w-8 rounded-xl bg-green-200">
-          </div>
-          <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
-            Explore
-          </div>
-        </div>
-        <div className="flex flex-row items-center text-2xl px-3 py-3 rounded-full font-semibold hover:bg-green-50">
-          <div className="h-8 w-8 rounded-xl bg-green-200">
-          </div>
-          <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
-          <Link to="/bookmarks">Bookmarks</Link>
-          </div>
-        </div>
-        <div className="flex flex-row items-center text-2xl px-3 py-3 rounded-full font-semibold hover:bg-green-50">
-          <div className="h-8 w-8 rounded-xl bg-green-200">
-          </div>
-          <div className="hidden leftShowLarge:ml-2 leftShowLarge:block">
-          <Link to={`/${username}`}>Profile</Link>
-          </div>
-        </div>
+    <div className="flex flex-col flex-grow items-end">
+      {/* used position fixed fix the sidebar to the top. But it removes the sidebar from doc flow and flex grow does not work*/}
+      {/* to get this working added a dummy element which has the same width properties as the sidebar. This makes flex grow work properly */}
+      <div className="leftShowLarge:items-stretch md:flex leftShowLarge:w-leftLarge w-leftSmall">
+      </div>
+      <div className="top-0 bottom-0 flex flex-col py-1 px-3 items-center leftShowLarge:items-stretch md:flex leftShowLarge:w-leftLarge w-leftSmall fixed">
+        <Link to="/posts" className="flex flex-row self-start p-3 rounded-full hover:bg-blue-100 ">
+          <div className="h-8 w-8 flex items-center justify-items-center">
+            <Logo/>
+          </div> 
+        </Link>
+        <LeftSidebarLink
+          Icon={<HomeIcon/>}
+          to="/posts"
+          Text="Home"
+        />
+        <LeftSidebarLink
+          Icon={<BookmarksIcon/>}
+          to="/bookmarks"
+          Text="Bookmarks"
+        />
+        <LeftSidebarLink
+          Icon={<ProfileIcon/>}
+          to="/profile"
+          Text="Profile"
+        />
       </div>
     </div>
   )
--- client/src/components/RightSidebar/index.js
+++ client/src/components/RightSidebar/index.js
@@ -17,99 +17,101 @@ const RightSidebar = () => {
   }
 
   return(
-    <div className="top-0 bottom-0 flex-col py-1 ml-7 mr-3 lg:items-start hidden rightShowSmall:flex rightShowSmall:w-rightSmall rightShowLarge:w-rightLarge ">
-      <div className="w-full">
-        <Formik
-        initialValues={initialValues}
-        onSubmit={(values) => {search(values)}}
-        > 
-          {() => (
-            <Form className="flex-col">
-              <Field name="search" placeholder="Search Twitter" component={SearchInput}/>
-              {/* <button className="rounded-md w-full bg-green-200 h-12 px-4" type="submit">Sign In</button> */}
-            </Form>
-          )}
-          
-        </Formik>
-      </div>
+    <div className="flex flex-col flex-grow items-start">
+      <div className="top-0 bottom-0 flex-col py-1 ml-7 mr-3 lg:items-start hidden rightShowSmall:flex rightShowSmall:w-rightSmall rightShowLarge:w-rightLarge ">
+        <div className="w-full">
+          <Formik
+          initialValues={initialValues}
+          onSubmit={(values) => {search(values)}}
+          > 
+            {() => (
+              <Form className="flex-col">
+                <Field name="search" placeholder="Search Twitter" component={SearchInput}/>
+                {/* <button className="rounded-md w-full bg-green-200 h-12 px-4" type="submit">Sign In</button> */}
+              </Form>
+            )}
+            
+          </Formik>
+        </div>
 
 
-      <div className="flex flex-col mt-3 rounded-xl bg-gray-50 w-full">
-        <div className="px-4 py-3 text-xl font-semibold border-b">
-          What's happening
-        </div>
-        <div className="px-4 py-3 border-b flex flex-row">
-          <div className="flex flex-col">
-            <div className="text-xs text-gray-500 mb-1">
-              <span>
-                News 
-              </span> 
-              <span className="pl-1">
-                . time
-              </span>
+        <div className="flex flex-col mt-3 rounded-xl bg-gray-50 w-full">
+          <div className="px-4 py-3 text-xl font-semibold border-b">
+            What's happening
+          </div>
+          <div className="px-4 py-3 border-b flex flex-row">
+            <div className="flex flex-col">
+              <div className="text-xs text-gray-500 mb-1">
+                <span>
+                  News 
+                </span> 
+                <span className="pl-1">
+                  . time
+                </span>
+              </div>
+              <div className="text-sm font-semibold">
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit,  
+                labore et dolore magna aliqua.
+              </div>
             </div>
-            <div className="text-sm font-semibold">
-              Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit,  
-              labore et dolore magna aliqua.
+            <div className="w-52 rounded-lg bg-green-100">
             </div>
           </div>
-          <div className="w-52 rounded-lg bg-green-100">
-          </div>
-        </div>
-        <div className="px-4 py-3 border-b flex flex-row justify-between">
-          <div className="flex flex-col">
-            <div className="text-xs text-gray-500 mb-1">
-              Trending in India
+          <div className="px-4 py-3 border-b flex flex-row justify-between">
+            <div className="flex flex-col">
+              <div className="text-xs text-gray-500 mb-1">
+                Trending in India
+              </div>
+              <div className="text-sm font-semibold mb-1">
+                #Lorem ipsum dolor sit
+              </div>
+              <div className="text-xs text-gray-500 mb-1">
+                Trending with something
+              </div>
             </div>
-            <div className="text-sm font-semibold mb-1">
-              #Lorem ipsum dolor sit
-            </div>
-            <div className="text-xs text-gray-500 mb-1">
-              Trending with something
-            </div>
-          </div>
-          <div className="">
-            button
-          </div>
-        </div>
-        <div className="px-4 py-3 border-b flex flex-row justify-between">
-          <div className="flex flex-col">
-            <div className="text-xs text-gray-500 mb-1">
-              Trending in India
-            </div>
-            <div className="text-sm font-semibold mb-1">
-              #Lorem ipsum dolor sit
-            </div>
-            <div className="text-xs text-gray-500 mb-1">
-              Trending with something
+            <div className="">
+              button
             </div>
           </div>
-          <div className="">
-            button
-          </div>
-        </div>
-        <div className="px-4 py-3 border-b flex flex-row">
-          <div className="flex flex-col">
-            <div className="text-xs text-gray-500 mb-1">
-              <span>
-                News 
-              </span> 
-              <span className="pl-1">
-                . time
-              </span>
+          <div className="px-4 py-3 border-b flex flex-row justify-between">
+            <div className="flex flex-col">
+              <div className="text-xs text-gray-500 mb-1">
+                Trending in India
+              </div>
+              <div className="text-sm font-semibold mb-1">
+                #Lorem ipsum dolor sit
+              </div>
+              <div className="text-xs text-gray-500 mb-1">
+                Trending with something
+              </div>
             </div>
-            <div className="text-sm font-semibold">
-              Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit,  
-              labore et dolore magna aliqua.
+            <div className="">
+              button
             </div>
           </div>
-          <div className="w-52 rounded-lg bg-green-100">
+          <div className="px-4 py-3 border-b flex flex-row">
+            <div className="flex flex-col">
+              <div className="text-xs text-gray-500 mb-1">
+                <span>
+                  News 
+                </span> 
+                <span className="pl-1">
+                  . time
+                </span>
+              </div>
+              <div className="text-sm font-semibold">
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit,  
+                labore et dolore magna aliqua.
+              </div>
+            </div>
+            <div className="w-52 rounded-lg bg-green-100">
+            </div>
           </div>
-        </div>
-        <div className="px-4 py-4 text-sm flex flex-row text-green-300">
-          Show more
+          <div className="px-4 py-4 text-sm flex flex-row text-green-300">
+            Show more
+          </div>
         </div>
       </div>
     </div>
