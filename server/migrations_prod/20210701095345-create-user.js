d1cbca61ed52bcafac87bf57882f1e223561d235
prajotsurey <prajotsurey@gmail.com>
2021-09-09 16:47:12 +0530

    updated migrations and migration path

---
+++ server/migrations_prod/20210701095345-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password_hash: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      name:{
+        type: Sequelize.TEXT,
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ server/migrations_prod/20210721063421-create-bookmarks.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('bookmarks', {
+      userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      postId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'posts',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('bookmarks');
+  }
+};
