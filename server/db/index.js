b3642a54805e626016de0d653f7b7097c896ecfc
prajotsurey <prajotsurey@gmail.com>
2021-09-09 17:18:58 +0530

    edited env variable name

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 const DATABASE = process.env.NODE_ENV === "test"
 ? process.env.TEST_DATABASE
-: process.env.DEV_DATABASE
+: process.env.DATABASE_URL
 
 
 const sequelize = new Sequelize(DATABASE,'postgres','password',{
