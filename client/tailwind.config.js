d3429df03d7a761ac08b96871251c639e020d014
prajotsurey <prajotsurey@gmail.com>
2021-09-09 17:51:21 +0530

    updated purge settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.vue',
-    './src/**/*.jsx',
+    './src/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
