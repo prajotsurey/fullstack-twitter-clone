625a22739ee90790bb09c7e7353a3fba7762cad5
prajotsurey <prajotsurey@gmail.com>
2021-09-10 21:04:05 +0530

    set centrespace height to 100vh

--- client/src/components/CenterSpace/index.js
+++ client/src/components/CenterSpace/index.js
@@ -6,7 +6,7 @@ const CentreSpace = (props) => {
   return(
     <div className="flex md:flex-row justify-center">
       <LeftSidebar />
-      <div className="flex flex-col border-r border-l">
+      <div className="flex flex-col min-h-screen h-full border-r border-l">
         <div className="w-center"> 
           {props.children}
         </div>
